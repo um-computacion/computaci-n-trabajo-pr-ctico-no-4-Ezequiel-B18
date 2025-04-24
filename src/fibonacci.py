fab6e3f9b84b8aad4b0d5941e245cdf8551be955
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-24 18:40:36 -0300

    Implementacion de funcion fibonacci iterative

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,7 +1,24 @@
-def fibonacci_iterative():
-    pass
+def fibonacci_iterative(num):
+    if num < 0:
+        raise ValueError('Fibonacci no esta definido para numeros negativos.')
 
-def fibonacci_recursive():
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    
+    fib_anterior = 0
+    fib_ahora = 1
+
+    for i in range(2, num + 1):
+        fib_siguiente = fib_anterior + fib_ahora
+
+        fib_anterior = fib_ahora
+        fib_ahora = fib_siguiente
+    
+    return fib_ahora
+
+def fibonacci_recursive(num):
     pass
 
 def main():
